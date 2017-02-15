e5d2ea4ed10fb1d9449c06588d76890d8113775e
Alexandr Yanenko <yanenkoalexandr@gmail.com>
2017-02-15 17:06:38 +0300

    Checking for file correctness, copyright

--- js/latexcheck.js
+++ js/latexcheck.js
@@ -1,7 +1,21 @@
+// Copyright (C) 2017 BarD Software s.r.o
+// Author: Alexandr Yanenko (yanenkoalexandr@gmail.com)
 var checkLatexCode = require("./main").checkLatexCode;
 var fs = require("fs");
 
+if (process.argv.length < 3) {
+    console.log("Input file is needed.");
+    process.exit();
+}
 var inputName = process.argv[2];
+if (!fs.existsSync(inputName)) {
+    console.log("Input file should exist.");
+    process.exit();
+}
+if (fs.lstatSync(inputName).isDirectory()) {
+    console.log("Input file should not be a directory.");
+    process.exit();
+}
 var outputName = process.argv[3] || "out.json";
 var contents = fs.readFileSync(inputName, "utf8");
 fs.writeFileSync(outputName, JSON.stringify(checkLatexCode(contents), null, 2));
